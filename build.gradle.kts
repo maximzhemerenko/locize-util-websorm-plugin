902930bed41b3936aba95036a59a3434f5a7f006
Maksym Zhemerenko <maxim.zhemerenko@icloud.com>
2025-09-03 23:16:20 +0200

    Update version commands

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.locizehelper"
-version = "1.2.0"
+version = "1.4.0"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/com/locizehelper/actions/LocizeDevSyncAction.kt
+++ src/main/kotlin/com/locizehelper/actions/LocizeDevSyncAction.kt
@@ -4,25 +4,28 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.ui.Messages
+import com.locizehelper.core.Release
 import com.locizehelper.core.TranslationUtil
 import com.locizehelper.core.getNamespace
 
-class LocizeDevSyncAction : AnAction() {
+class LocizeDevSyncAction(private val release: Release? = null) : AnAction(if (release == null) "Locize DEV SYNC" else "DEV SYNC") {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         val file = e.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
 
         val namespace = getNamespace(file)
 
+        val versionDescription = if (release == null) "" else "(${release.product} ${release.version})"
+
         if (Messages.showYesNoDialog(
                 e.project,
-                "Do you really want to overwrite the \"$namespace\" namespace with your local version? This will remove any translations that may have been added by other developers.",
+                "Do you really want to overwrite the \"$namespace\" namespace $versionDescription with your local version? This will remove any translations that may have been added by other developers.",
                 "DEV SYNC",
                 Messages.getQuestionIcon()
             ) != Messages.YES) {
             return
         }
 
-        TranslationUtil(project).run("dev", namespace, sync = true)
+        TranslationUtil(project).run("dev", namespace, release = release, sync = true)
     }
 }
